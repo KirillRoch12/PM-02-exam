ef9d4a237db78f9c0cde8d5dfebb9287e5f51201
Gorbusin Kirill <nfs-world.2000-2014@mail.ru>
2020-06-30 10:20:11 +0500

    Готовый проект

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -5,10 +5,24 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Menus;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure KeyPress(Sender: TObject; var Key: char);
   private
 
   public
@@ -19,8 +33,34 @@ var
   Form1: TForm1;
 
 implementation
+uses Unit2;
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button2Click(Sender: TObject);
+var a,d,c:real;
+begin
+  a:=StrToFloat(Edit1.text);
+  d:=StrToFloat(Edit2.text);
+  c:=a/d;
+  Edit3.text:=FloatToStr( c );
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Unit2.Form2.Showmodal;
+end;
+
+procedure TForm1.KeyPress(Sender: TObject; var Key: char);
+begin
+if not (Key in [#8,'0'..'9',',']) then
+begin
+MessageDlg('Ошибка ввода, недопустимый символ',mtInformation,[mbOk],0);
+Key:=#0;
+end;
+end;
 
 end.
 
--- unit1.pas
+++ unit1.pas
@@ -5,7 +5,7 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Menus;
 
 type
 
@@ -20,6 +20,9 @@ type
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure KeyPress(Sender: TObject; var Key: char);
   private
 
   public
@@ -30,8 +33,34 @@ var
   Form1: TForm1;
 
 implementation
+uses Unit2;
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button2Click(Sender: TObject);
+var a,d,c:real;
+begin
+  a:=StrToFloat(Edit1.text);
+  d:=StrToFloat(Edit2.text);
+  c:=a/d;
+  Edit3.text:=FloatToStr( c );
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Unit2.Form2.Showmodal;
+end;
+
+procedure TForm1.KeyPress(Sender: TObject; var Key: char);
+begin
+if not (Key in [#8,'0'..'9',',']) then
+begin
+MessageDlg('Ошибка ввода, недопустимый символ',mtInformation,[mbOk],0);
+Key:=#0;
+end;
+end;
 
 end.
 
